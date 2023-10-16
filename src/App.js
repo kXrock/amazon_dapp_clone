5f12ccaece9aec4a0d2e0b021a96d0c25dcc22d7
kXrock <keremkaya08@hotmail.com>
2023-10-16 21:28:47 +0300

    connect wallet done

--- src/App.js
+++ src/App.js
@@ -14,10 +14,22 @@ import config from './config.json'
 
 function App() {
 
+const loadBlockchaindata =async()=>{
+  const accounts= await window.ethereum.request({method:"eth_requestAccounts"});
+  const account= ethers.utils.getAddress(accounts[0]);
+  console.log(account);
+}
+
+/*useEffect(()=>{
+  loadBlockchaindata()
+},[])*/
+
+
   return (
     <div>
 
       <h2>Welcome to Dappazon</h2>
+      <button onClick={loadBlockchaindata}>connect wallet</button>
 
     </div>
   );
