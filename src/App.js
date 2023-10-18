b66836bfd7ebb540ca1d336ad5409836fc5912ad
kXrock <keremkaya08@hotmail.com>
2023-10-18 13:52:44 +0300

    added blockchain data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect,useState } from 'react'
 import { ethers } from 'ethers'
 
 // Components
@@ -14,12 +14,22 @@ import config from './config.json'
 
 function App() {
   const [account,setAccount]= useState(null);
+  const [provider,setProvider] =useState();
 
 const loadBlockchaindata =async()=>{
-  const accounts= await window.ethereum.request({method:"eth_requestAccounts"});
-  const account= ethers.utils.getAddress(accounts[0]);
-  setAccount(account);
+  const provider= new ethers.providers.Web3Provider(window.ethereum);
+  setProvider(provider);
+
+  const network= await provider.getNetwork();
+  console.log(network);
+
+  const dappazon = new ethers.Contract(0x5FbDB2315678afecb367f032d93F642f64180aa3,Dappazon,provider)
 }
+
+useEffect(()=>{
+  loadBlockchaindata();
+},[])
+
   return (
     <div>
       <Navigation account={account} setAccount={setAccount}></Navigation>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,6 +1,12 @@
 import { ethers } from 'ethers';
 
-const Navigation = ({ account, setAccount }) => {
+const Navigation = ({ account, setAccount, loadBlockchaindata }) => {
+
+    const connectHandler = async () => {
+        const accounts = await window.ethereum.request({ method: "eth_requestAccounts" });
+        const account = ethers.utils.getAddress(accounts[0]);
+        setAccount(account);
+    }
 
     return (
         <nav>
@@ -11,15 +17,18 @@ const Navigation = ({ account, setAccount }) => {
                 type='text'
                 className='nav__search'>
             </input>
-            <button type='button' className='nav__connect'>
-                {account.slice(0, 6) + "..." + account.slice(38, 42)}
-            </button>
+            {account ? (
+                <button type='button' className='nav__connect'>
+                    {account.slice(0, 6) + "..." + account.slice(38, 42)}
+                </button>) : (<button type='button' className='nav__connect' onClick={connectHandler}>
+                    Connect
+                </button>)}
+
             <ul className='nav__links'>
                 <li><a href="#Clothing & Jewelry">Clothing & Jewelry</a></li>
                 <li><a href="#Electronics & Gadgets">Electronics & Gadgets</a></li>
                 <li><a href="#Toys & Gaming">Toys & Gaming</a></li>
             </ul>
-            
         </nav>
     );
 }
