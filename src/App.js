3db9ec879e1dbe80c61073836aaddff0e5b6d731
kXrock <keremkaya08@hotmail.com>
2023-10-18 21:16:31 +0300

    item added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect,useState } from 'react'
+import { useEffect, useState } from 'react'
 import { ethers } from 'ethers'
 
 // Components
@@ -12,23 +12,31 @@ import Dappazon from './abis/Dappazon.json'
 // Config
 import config from './config.json'
 
-function App() {
-  const [account,setAccount]= useState(null);
-  const [provider,setProvider] =useState();
+async function App() {
+  const [account, setAccount] = useState(null);
+  const [provider, setProvider] = useState();
+  const [dappazon, setdappazon] = useState();
 
-const loadBlockchaindata =async()=>{
-  const provider= new ethers.providers.Web3Provider(window.ethereum);
-  setProvider(provider);
+  const loadBlockchaindata = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    setProvider(provider);
 
-  const network= await provider.getNetwork();
-  console.log(network);
+    const network = await provider.getNetwork();
+    console.log(network);
 
-  const dappazon = new ethers.Contract(0x5FbDB2315678afecb367f032d93F642f64180aa3,Dappazon,provider)
-}
+    const dappazon = new ethers.Contract(config[network.chainId].dappazon.address, Dappazon, provider)
+    setdappazon(dappazon);
+  }
 
-useEffect(()=>{
-  loadBlockchaindata();
-},[])
+  const items = [];
+  for (var i = 1; i <= 9; i++) {
+    const item= await dappazon.items[i];
+    items.push(item);
+  }
+
+  useEffect(() => {
+    loadBlockchaindata();
+  }, [])
 
   return (
     <div>
